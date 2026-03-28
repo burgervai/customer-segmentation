eb2f163a535cc005b328dba3e4e93f1b0dfbc53b
burgervai <88193069+burgervai@users.noreply.github.com>
2026-03-29 01:18:07 +0600

    Update init.py

--- src/pipeline/init.py
+++ src/pipeline/init.py
@@ -1,2 +1,2 @@
-from pipeline.predict_pipeline import PredictPipeline, CustomData
-from pipeline.train_pipeline import TrainPipeline
+from .predict_pipeline import PredictPipeline
+from .train_pipeline import TrainPipeline
